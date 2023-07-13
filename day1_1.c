1e79d5f2b4a8308a931dd71393b02d4314e2786a
76001-Rahulkashid <98150840+76001-Rahulkashid@users.noreply.github.com>
2023-07-13 21:23:32 +0530

    Add files via upload

---
+++ day1_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // standard input output header file
+
+int main() // entry point function : user defined function
+{
+    printf("Hello PH18 !"); // printf is predefined function
+    return 0; // exit status - success
+
+}
+
+/*
+multi-line comments
+void main()
+{
+    printf("Hello !");
+}
+
+*/
